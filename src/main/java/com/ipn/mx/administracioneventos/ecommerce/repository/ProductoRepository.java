940a4734e35720a554088ba450808694c14d6e1c
SergioRodriguez <147294346+decsefer@users.noreply.github.com>
2026-01-06 20:48:29 -0600

    Update ProductoRepository.java

--- src/main/java/com/ipn/mx/administracioneventos/ecommerce/repository/ProductoRepository.java
+++ src/main/java/com/ipn/mx/administracioneventos/ecommerce/repository/ProductoRepository.java
@@ -4,9 +4,10 @@ import com.ipn.mx.administracioneventos.ecommerce.domain.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
     List<Producto> findByCategoria_IdCategoria(Long idCategoria);
     List<Producto> findByNombreContainingIgnoreCase(String q);
+    Optional<Producto> findByNombre(String nombre);
 }
-
