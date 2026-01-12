e23951d203895b0cc34caae3bad5d0ef5040ae8f
SergioRodriguez <147294346+decsefer@users.noreply.github.com>
2026-01-12 00:38:02 -0600

    Update CarritoItemRepository.java

--- src/main/java/com/ipn/mx/administracioneventos/ecommerce/repository/CarritoItemRepository.java
+++ src/main/java/com/ipn/mx/administracioneventos/ecommerce/repository/CarritoItemRepository.java
@@ -2,6 +2,16 @@ package com.ipn.mx.administracioneventos.ecommerce.repository;
 
 import com.ipn.mx.administracioneventos.ecommerce.domain.CarritoItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface CarritoItemRepository extends JpaRepository<CarritoItem, Long> {}
+import java.util.List;
+import java.util.Optional;
 
+public interface CarritoItemRepository extends JpaRepository<CarritoItem, Long> {
+    List<CarritoItem> findByCarrito_IdCarrito(Long idCarrito);
+    Optional<CarritoItem> findByCarrito_IdCarritoAndProducto_IdProducto(Long idCarrito, Long idProducto);
+    @Transactional
+    void deleteByCarrito_IdCarritoAndProducto_IdProducto(Long idCarrito, Long idProducto);
+    @Transactional
+    void deleteByCarrito_IdCarrito(Long idCarrito);
+}
